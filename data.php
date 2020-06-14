2751db8f950f74110288b648e878dde508293908
adysetyawan <ady1800016092@uad.ac.id>
2020-06-14 14:31:57 +0700

    latihan 1 dan 2 modul 4 php

---
+++ data.php
@@ -0,0 +1,49 @@
+<?php
+$NIM=$_POST['NIM'];
+$NAMA=$_POST['NAMA'];
+$KELAMIN=$_POST['KELAMIN'];
+$ALAMAT=$_POST['ALAMAT'];
+$PRODI=$_POST['PRODI'];
+$BAHASA=$_POST['BAHASA'];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Data Mahasiswa</title>
+    </head>
+<body>
+    <table width="500" border="1">
+        
+        <tr>
+                <td>NIM</td>
+                <td><?php echo $NIM; ?></td>
+        </tr>
+
+        <tr>
+                <td>Nama</td>
+                <td><?php echo $NAMA; ?></td>
+        </tr>
+
+        <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $KELAMIN; ?></td>
+        </tr>
+
+        <tr>
+               <td>ALAMAT</td>
+                <td><?php echo$ALAMAT; ?></td>
+        </tr>
+
+         <tr>
+               <td>Program Studi</td>
+                <td><?php echo$PRODI; ?></td>
+        </tr>
+
+         <tr>
+               <td>Bahasa Pemrograman yang Dikuasai</td>
+                <td><?php echo$BAHASA; ?></td>
+        </tr>
+    </table>
+</body>
+<html>
